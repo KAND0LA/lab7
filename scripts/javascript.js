07a548c649d1923814ec8984b2b7d48d2a6532da
Devin Kandola <dkandola5@my.bcit.ca>
2022-11-11 22:27:22 -0800

    created user input, array, and while loop

---
+++ scripts/javascript.js
@@ -0,0 +1,12 @@
+const courseList = [
+    {code: "ACIT 1420", name: "Introduction to Systems Adminstration"},
+    {code: "ACIT 1620", name: "Web Fundamental Technologies"},
+    {code: "ACIT 1630", name: "Database Systems"},
+]
+
+var digit = prompt("What is the code?");
+
+while (digit)
+    if (digit === 1420, 1620, 1630) {
+        console.log(Number("Yes I am taking the course"))
+    }
